c35f584310bc906676006720d8ffdffe55226653
obinnayaonyinyechi <obinnayaonyinyechi@gmail.com>
2024-04-19 21:11:31 +0400

    portfolio code

---
+++ src/app/data.js
@@ -0,0 +1,146 @@
+// this comtains the data we are gonna for AboutMe component and slider component
+
+export const AboutData = [
+  // {
+  //   count: 1,
+  //   title: "UI / UX Designer",
+  //   img: "/first.svg",
+  //   array: [
+  //     "Information Architecture",
+  //     "Pen and Paper",
+  //     "Adobe Photoshop",
+  //     "Adobe Illustrator",
+  //     "Figma",
+  //     "Framer",
+  //     "Typography / Spacing / ...",
+  //   ],
+  // },
+  {
+    count: 1,
+    title: "Front-end Developer",
+    img: "/first.svg",
+    array: [
+      "HTML / CSS3",
+      "javaScript (ES6)",
+      "React.js",
+      "Next.js",
+      "Tailwind CSS",
+      "Bootstrap",
+      "REST API / GraphQL",
+    ],
+  },
+  {
+    count: 2,
+    title: "Back-end Developer",
+    img: "/second.svg",
+    array: [
+      "Node.js / Express.js",
+      "Django / Flask",
+      "MongoDB / MySQL",
+      "Next.js (API)",
+    ],
+  },
+  {
+    count: 3,
+    title: "Data Science",
+    img: "/third.svg",
+    array: [
+      "Python Programming",
+      "Numpy / Pandas",
+      "EDA",
+      "Data Analytics",
+      "Data Visualization",
+      "Graphical Representation",
+      "Tabulae / Power BI / Seaborn / Matplotlib",
+    ],
+  },
+  {
+    count: 4,
+    title: "Machine Learning",
+    img: "/fourth.svg",
+    array: [
+      "Scikit-Learn",
+      "KNN / SVM",
+      "Linear Regression",
+      "K-Means Clustering",
+      "Logistic Regression / Classification",
+      "Decision Trees",
+      "Deployment Using Flask",
+    ],
+  },
+  {
+    count: 5,
+    title: "Deep Learning / AI",
+    img: "/fifth.svg",
+    array: [
+      "Keras",
+      "Recurrent Neural Networks (RNN)",
+      "Convolutional Neural Networks (CNN)",
+      "Natural Language Processing (NLP)",
+      "Computer Vision",
+      "Deployment Using Flask",
+    ],
+  },
+  {
+    count: 6,
+    title: "Tools and Things",
+    img: "/sisxth.svg",
+    array: [
+      "VSCode",
+      "Git / Github",
+      "Bash / Powershell",
+      "NPM / Yarn",
+      "Postman",
+    ],
+  },
+];
+
+// this is for the slider
+export const DataArray = [
+  {
+    title: "Next.js Portfolio Website",
+    name: "Next.js Portfolio Website",
+    des: "I have made this project using React/Next.js to showcase some of my projects to clients or employers. The design of this portfolio was taken from a friend. It was carefully and professionally design to work in all devices",
+    des1: "This project was made using, React.js, Next.js, NodeMailer, Tailwind css  and many more!",
+    images: ["/portfolioimage.png", "/portfolioimage1.png"],
+  },
+  {
+    title: "Food app Website Using React.js",
+    name: "Eat App",
+    des: "I have made this Eat App project using React.js, Tailwind css, Vite and more to showcase it to clients and employers.",
+    des1: "This project was made using, React.js, Next.js, Express.js, Node.js and many more!",
+    images: ["/eat-app.png"],
+  },
+  {
+    title: "Netflix clone",
+    name: "Netflix clone App",
+    des: "I clone this Netflix App.",
+    des1: "",
+
+    images: ["/netflix clone.png", "/netflix clone1.png"],
+  },
+  {
+    title: "Real Estate and Property Management Project",
+    name: "Inans Property Management ",
+    des: "We built this project for inans group LTD",
+    des1: "Aside from its gorgeous design, this website is backed by an outstanding plugin that allows owners and agencies to simply display different properties in the neighbourhood and create an online showcase in an appealing manner for home buyers and tenants..",
+
+    images: ["/property1.png"],
+  },
+  {
+    title: "Crypto Trading Website",
+    name: "Crypto Trading project",
+    des: "We buit this website for BigMoney group",
+    des1: "This project was built to enable clients all over the world to buy and sell their crypto.",
+
+    images: ["/crypto5.png", "/crypto3.png", "/crypto1.png"],
+  },
+  {
+    title: "Food Delivery Company WebSite",
+    name: "Yum Food Company",
+    des: "We did  this project for Yum Foods Located in USA.",
+    des1: "This website's graphic design is fantastic. Animated sliders are painstakingly built and created in WordPress with Revolution Slider. All of the product photos are photo manipulations created in Adobe Photoshop.",
+
+    images: ["/yumfood.jpg", "/deliveryguy.png"],
+  },
+];
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,26 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Recoleta-Black.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Recoleta-Medium.ttf",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/Recoleta-Light.ttf",
+      weight: "200",
+    },
+  ],
+  variable: "--font-Recoleta-Black",
+});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +30,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header/>
+      {children}
+      <Footer/>
+      </body>
     </html>
   );
 }
